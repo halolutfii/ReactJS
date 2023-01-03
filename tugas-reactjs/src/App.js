a38500b2fe679fdc0d0b728daa4e901e8efbe462
halolutfii <halolutfii@gmail.com>
2023-01-04 05:04:46 +0700

    Tugas 7 - React:Introduction

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Container">
+      <h1 className='name'>Hi, Saya Lutfi Cahya Nugraha</h1>
+      <p className='garis'></p>
+      <p className='paragraf'>Saya dari Batch 41 sudah berhasil menginstall React</p>
+    </div>
+  );
+}
+
+export default App;
